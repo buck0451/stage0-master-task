4090ce9b131d46e8e44c9889ddd74a130e17bd9c
buck0451 <buck0451@yandex.com>
2023-10-21 18:26:09 +0300

    Loops

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,7 +3,11 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
-
+        int ans = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            ans *= numberToPrint;
+        }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,6 +3,15 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+        if (cathetusLength > 0) {
+            String s = "1";
+            System.out.println(" ".repeat(cathetusLength - 1) + s);
+            for (int i = 2; i <= cathetusLength; i++) {
+                s = i + s + i;
+                System.out.println(" ".repeat(cathetusLength - i) + s);
+            }
+        }
+
 
     }
 
