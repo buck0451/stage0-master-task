6794423dd42df0442f98e3cb8ea960ca2682490c
buck0451 <buck0451@yandex.com>
2023-10-21 13:59:47 +0300

    DigitsSumCalculator + NumberReverter

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int newNum = 0;
+        for (int i = 2; i > -1; i--) {
+            newNum += (int) (Math.pow(10, i) * (number % 10));
+            number /= 10;
+        }
+        System.out.println(newNum);
     }
 
 }
